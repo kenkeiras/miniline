94a40ad23614df1d5e24a9a148faa58b638f9045
Sergio Martinez Portela <sergio.mportela@gmail.com>
2017-11-20 09:51:04 +0100

    Show cluster name in PS1.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod symbols;
 mod segments;
 
 use std::env;
+use std::process::Command;
 
 use segments::connection::connect_last;
 use segments::connection::connect_segments;
@@ -20,6 +21,21 @@ fn main() {
     let first_segment : String = error_segment(error);
     let virtualenv_segment : Option<String> = virtualenv_segment();
     let second_segment : String = path_segments();
+    let context = Command::new("kubectl")
+        .arg("config")
+        .arg("current-context")
+        .output()
+        .expect("failed to execute process").stdout;
+
+    let cluster = String::from_utf8_lossy(&context);
+    let cluster = cluster.trim();
+
+    let cluster_bg = if cluster == "minikube" {
+        "2"
+    }
+    else {
+        "1"
+    };
 
     print!("{clean}{error} ",
              clean=clean_colors,
@@ -33,16 +49,24 @@ fn main() {
                    c1=connect_segments(colors::LAST_RESULT_BG,
                                        colors::VIRTUALENV_BG),
                    c2=connect_segments(colors::VIRTUALENV_BG,
-                                       colors::PATH_BG),
+                                       cluster_bg),
                    )
         }
         None => {
             print!("{c}",
                      c=connect_segments(colors::LAST_RESULT_BG,
-                                        colors::PATH_BG),
+                                        cluster_bg),
                      )
         }
     }
+
+
+    print!("{color_base}{cluster_color}{color_end} {cluster} {connector}",
+           color_base="\\[\x1b[1;37m\x1b[1;4",
+           cluster_color=cluster_bg,
+           color_end="m\\]",
+           cluster=cluster,
+           connector=connect_segments(cluster_bg, colors::PATH_BG));
 
     print!("{path} {c}{clean} ",
              c=connect_last(colors::PATH_BG),
